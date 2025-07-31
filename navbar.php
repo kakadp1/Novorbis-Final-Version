86ab67c0af37abe4f3e6666f835e3727f3b61fcc
Prathamesh Kakad <73532295+kakadp1@users.noreply.github.com>
2025-08-01 00:14:20 +0530

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,14 +1,17 @@
 <!-- header area -->
 <div class="rts-header header-style-nine">
-    <header class="header-four header--sticky colorChangeForNavbar" style="border-bottom:1px solid #eee;">
+
+
+    <header class="header-four header--sticky">
         <div class="header-left">
             <a href="index.php">
-                <img src="assets/images/logo.png" alt="Novorbis Itus Pvt Ltd Logo" class="margin-top: 5px;">
+                <img src="assets/images/logo2.png" alt="Novorbis Itus Pvt Ltd Logo" class="margin-top: 5px;">
             </a>
         </div>
         <div class="nav-area">
+
             <!-- navigation area start -->
-            <div class="header-nav main-nav-one">
+            <div class="header-nav main-nav-one navBarPosition">
                 <nav>
                     <?php
                     $currentPage = basename($_SERVER['PHP_SELF']);
@@ -16,31 +19,31 @@
 
                     <ul>
                         <li class="<?= ($currentPage == 'index.php') ? 'active' : '' ?>">
-                            <a class="nav-link" href="index.php">Home</a>
+                            <a class="nav-link individualPadding" href="index.php">Home</a>
                         </li>
                         <li class="<?= ($currentPage == 'about.php') ? 'active' : '' ?>">
-                            <a class="nav-link" href="about.php">About Us</a>
+                            <a class="nav-link individualPadding" href="about.php">About Us</a>
                         </li>
                         <li
                             class="has-dropdown <?= ($currentPage == 'carbon-free-recds.php' || $currentPage == 'carbon-free-caps.php') ? 'active' : '' ?>">
-                            <a class="nav-link" href="#">Products</a>
+                            <a class="nav-link individualPadding" href="#">Products</a>
                             <ul class="submenu">
                                 <li class="<?= ($currentPage == 'carbon-free-recds.php') ? 'active' : '' ?>">
-                                    <a href="carbon-free-recds.php">Carbonfree RECD</a>
+                                    <a href="carbon-free-recds.php">RECDs</a>
                                 </li>
                                 <li class="<?= ($currentPage == 'carbon-free-caps.php') ? 'active' : '' ?>">
-                                    <a href="carbon-free-caps.php">Carbonfree CAPS</a>
+                                    <a href="carbon-free-caps.php">CAPS</a>
                                 </li>
                             </ul>
                         </li>
                         <li class="<?= ($currentPage == 'regulations.php') ? 'active' : '' ?>">
-                            <a class="nav-link" href="regulations.php">Regulations</a>
+                            <a class="nav-link individualPadding" href="regulations.php">Regulations</a>
                         </li>
                         <li class="<?= ($currentPage == 'media.php') ? 'active' : '' ?>">
-                            <a class="nav-link" href="media.php">Media</a>
+                            <a class="nav-link individualPadding" href="media.php">Media</a>
                         </li>
                         <li class="<?= ($currentPage == 'contact.php') ? 'active' : '' ?>">
-                            <a class="nav-link" href="contact.php">Contact</a>
+                            <a class="nav-link individualPadding" href="contact.php">Contact</a>
                         </li>
                     </ul>
                 </nav>
@@ -52,25 +55,24 @@
                 <!-- <a href="#" class="contact">Let’s Talk</a> -->
                 <div id="menu-btn">
                     <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <rect width="4" height="4" fill="black" />
-                        <rect y="7" width="4" height="4" fill="black" />
-                        <rect y="14" width="4" height="4" fill="black" />
-                        <rect x="7" width="4" height="4" fill="black" />
-                        <rect x="7" y="7" width="4" height="4" fill="black" />
-                        <rect x="7" y="14" width="4" height="4" fill="black" />
-                        <rect x="14" width="4" height="4" fill="black" />
-                        <rect x="14" y="7" width="4" height="4" fill="black" />
-                        <rect x="14" y="14" width="4" height="4" fill="black" />
+                        <rect width="4" height="4" fill="white" />
+                        <rect y="7" width="4" height="4" fill="white" />
+                        <rect y="14" width="4" height="4" fill="white" />
+                        <rect x="7" width="4" height="4" fill="white" />
+                        <rect x="7" y="7" width="4" height="4" fill="white" />
+                        <rect x="7" y="14" width="4" height="4" fill="white" />
+                        <rect x="14" width="4" height="4" fill="white" />
+                        <rect x="14" y="7" width="4" height="4" fill="white" />
+                        <rect x="14" y="14" width="4" height="4" fill="white" />
                     </svg>
                 </div>
             </div>
         </div>
     </header>
-    <!-- added underline and space between underline -->
     <style>
         .header-nav .active>a.nav-link,
         .header-nav .submenu .active>a {
-            color: #35a356 !important;
+            color: #FFFFFF !important;
             font-weight: bold;
             text-decoration: underline !important;
             text-underline-offset: 4px;
@@ -81,9 +83,21 @@
         .header-nav .submenu a {
             /* text-decoration: none !important; */
         }
+
+        header.header-four .nav-area .main-nav-one ul li.has-dropdown a.nav-link {
+            color: #fff;
+        }
+
+        header.header-four .nav-area .main-nav-one ul li a.nav-link {
+            color: #fff;
+        }
+
+        .header-four.header--sticky.sticky {
+            background-color: rgb(0 0 0 / 99%);
+            opacity: 0.7;
+            /* background: var(--color-primary); */
+        }
     </style>
-
-
 
     <!-- Floating WhatsApp & Call Buttons -->
     <div class="floating-contact">
@@ -136,3 +150,4 @@
             max-width: 150px;
         }
     </style>
+</div>
